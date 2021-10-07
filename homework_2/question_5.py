71a3d629b2f23db31ff92528b79e0060cfab0c92
Yonghao Zhou <yxz1754@case.edu>
2021-10-06 23:03:22 -0400

    add question sample generation

--- homework_2/question_5.py
+++ homework_2/question_5.py
@@ -49,11 +49,13 @@ def fit_power(data):
     a, loc, scale = powerlaw.fit(data)
     x = np.linspace(np.unique(data)[0], np.unique(data)[-1], bins)
     fit_data = powerlaw.pdf(x, a=a)
+    print('power alpha:', "{:.3f}".format(a))
+    print('powerlaw samples:', powerlaw.rvs(a, loc=loc, scale=scale, size=10))
     plt.plot(x, fit_data, 'k')
     if len(data) == 3409:
-        plt.savefig('airport_power_fit.png')
+        plt.savefig('./question_5/airport_power_fit.png')
     else:
-        plt.savefig('movie_power_fit.png')
+        plt.savefig('./question_5/movie_power_fit.png')
 
 def fit_exp(data):
     plt.clf()
@@ -62,11 +64,13 @@ def fit_exp(data):
     loc, scale = expon.fit(data)
     x = np.linspace(np.unique(data)[0], np.unique(data)[-1], bins)
     fit_data = expon.pdf(x, loc, scale)
+    print('exponential lambda:', "{:.3f}".format(1/scale))
+    print('exponential samples:', expon.rvs(loc=loc, scale=scale, size=10))
     plt.plot(x, fit_data, 'k')
     if len(data) == 3409:
-        plt.savefig('airport_expon_fit.png')
+        plt.savefig('./question_5/airport_expon_fit.png')
     else:
-        plt.savefig('movie_expon_fit.png')
+        plt.savefig('./question_5/movie_expon_fit.png')
 
 def fit_uni(data):
     plt.clf()
@@ -75,11 +79,13 @@ def fit_uni(data):
     loc, scale = uniform.fit(data)
     x = np.linspace(np.unique(data)[0], np.unique(data)[-1], bins)
     fit_data = uniform.pdf(x, loc, scale)
+    print('uniform a, b:', "{:.3f}".format(loc), "{:.3f}".format(loc + scale))
+    print('uniform samples:', uniform.rvs(loc=loc, scale=scale, size=10))
     plt.plot(x, fit_data, 'k')
     if len(data) == 3409:
-        plt.savefig('airport_uni_fit.png')
+        plt.savefig('./question_5/airport_uni_fit.png')
     else:
-        plt.savefig('movie_uni_fit.png')
+        plt.savefig('./question_5/movie_uni_fit.png')
 
 def fit_norm(data):
     plt.clf()
@@ -88,22 +94,29 @@ def fit_norm(data):
     loc, scale = norm.fit(data)
     x = np.linspace(np.unique(data)[0], np.unique(data)[-1], bins)
     fit_data = norm.pdf(x, loc, scale)
+    print('normal mean and std:', "{:.3f}".format(loc), "{:.3f}".format(scale))
+    print('uniform samples:', norm.rvs(loc=loc, scale=scale, size=10))
     plt.plot(x, fit_data, 'k')
     if len(data) == 3409:
-        plt.savefig('airport_norm_fit.png')
+        plt.savefig('./question_5/airport_norm_fit.png')
     else:
-        plt.savefig('movie_norm_fit.png')
+        plt.savefig('./question_5/movie_norm_fit.png')
 
 read_file()
 # print some statistics
 print(len(route_list), 'airports:\n\t', 'min conn:', np.min(route_list), 'max conn', np.max(route_list))
 print(len(rating_list), 'movies:\n\t', 'min rate:', np.min(rating_list), 'max rate', np.max(rating_list))
+print('')
 
 # fit airport
+print('---------------------------------')
+print('airport distribution parameters:')
 fit_power(route_list)
 fit_exp(route_list)
 fit_uni(route_list)
 fit_norm(route_list)
+print('---------------------------------')
+print('movie distribution parameters:')
 # fit movie
 fit_power(rating_list)
 fit_exp(rating_list)
